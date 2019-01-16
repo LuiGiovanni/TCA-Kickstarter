78be5f4fb5b8f27bb801a10b9a4df137dc845d03
Giovanni Lopez <giovanni.lopez3523@gmail.com>
2019-01-15 21:24:21 -0700

    More changes to cajero, some doubts exist on how tranfers between account types work, will check tomorrow

--- 1st Week/cajero/BankAccounts.java
+++ 1st Week/cajero/BankAccounts.java
@@ -1,6 +1,31 @@
-public class BankAccounts{
+import java.util.Random;
+
+public class BankAccounts extends Bank{
+    Random rand = new Random();
+    public int money;
     public int bankNumber;
     public String accountType;
+    public double balance;
+    public double creditLimit;
+    public int accountLevel;
+
+    public BankAccounts(String accountType, int accountLevel){
+        super(accountType,accountLevel);
+        this.accountType = accountType;
+
+        if(accountType == "Debit"){
+            bankNumber = rand.nextInt(200000) + 100000;
+            balance = 0;
+        }else if(accountType == "Credit"){
+            bankNumber = rand.nextInt(200000) + 100000;
+            creditLimit = 1000 * accountLevel;
+        }
+        
+    }
+
+    public void addFunds(int money){
+        balance
+    }
 
     
 }
